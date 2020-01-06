b530e68b36f3bca346b03f3070df080adbd9eb08
Faycca <52404727+Faycca@users.noreply.github.com>
2020-01-06 22:29:31 +0100

    first commit

---
+++ app/Http/Controllers/DevisController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DevisController extends Controller
+{
+    public function create(Request $request){
+
+        $devis = new \Devis();
+
+        $devis->
+
+
+    }
+}
